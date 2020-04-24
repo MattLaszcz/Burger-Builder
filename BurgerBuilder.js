c3f1ef32b982e7e821c69f265126f58330c245d5
Matthew Laszcz <matthewlaszcz@gmail.com>
2020-04-24 10:26:10 -0400

    Add files via upload

---
+++ BurgerBuilder.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients :{
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    },
+    totalPrice: 4
+  }  
+
+  addIngredientHandler = (type) => {
+      const oldCount = this.state.ingredients[type];
+      const updatedCounted = oldCount + 1;
+      const updatedIngredients = {
+        ...this.state.ingredients
+      };
+      updatedIngredients[type] = updatedCount;
+      const priceAddiction = INGREDIENT_PRICES[type];
+      const oldPrice = this.state.totalPrice;
+      const newPrice = oldPrice + priceAddiction;
+      this.setState ({totalPrice: newPrice, ingredients: updatedIngredients})
+  }
+
+
+  
+  render () {
+      return(
+        <Aux>
+          < Burger ingredients = {this.state.ingredients} />
+          <BuildControls 
+            ingredientAdded = {this.addIngredientHandler}
+          />
+        </Aux>
+      );
+    }
+}
+
+export default BurgerBuilder;
